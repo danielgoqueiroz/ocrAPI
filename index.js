00388fa692afd9e3c18d250b86ff6ef307770cb9
Daniel Gomes Queiroz <contato@danielqueiroz.com>
2020-08-29 20:36:57 -0300

    Upload de imagem via post

--- index.js
+++ index.js
@@ -1,20 +1,58 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const morgan = require("morgan");
+const TMP_FOLDER = "uploads/";
+const fs = require("fs");
 const app = express();
+
 const PORT = 5000;
 
 const getWorker = require("tesseract.js-node");
 
-app.get("/", async (req, res) => {
-  let dataInicial = new Date();
-  const worker = await getWorker({
-    tessdata: "./tessdata", // where .traineddata-files are located
-    languages: ["por"], // languages to load
-  });
-  const text = await worker.recognize("imagens/imagem_2.png", "por");
-  return res.send({
-    text: text,
-    duracao: (new Date().getTime() - dataInicial.getTime()) / 1000,
-  });
+app.use(
+  fileUpload({
+    createParentPath: true,
+  })
+);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.post("/", async (req, res) => {
+  console.log(req.files.imagem);
+  let imagem = null;
+  try {
+    if (!req.files) {
+      return res.send({
+        status: false,
+        message: "imagem não enviada",
+      });
+    } else {
+      imagem = req.files.imagem;
+      imagem.mv(TMP_FOLDER + imagem.name);
+
+      let dataInicial = new Date();
+      const worker = await getWorker({
+        tessdata: "./tessdata",
+        languages: ["por"],
+      });
+      const text = await worker.recognize(imagem.data, "por");
+      return res.send({
+        text: text.replace(/\s+/g, " ").trim(),
+        duracao: (new Date().getTime() - dataInicial.getTime()) / 1000,
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send(err);
+  } finally {
+    if (fs.existsSync(TMP_FOLDER + imagem)) {
+      console.log("Removendo arquivo de imagem temporário");
+      fs.unlinkSync(TMP_FOLDER + imagem.name);
+    }
+  }
 });
 
 app.listen(process.env.PORT || PORT, () =>
